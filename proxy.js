68ec421ffce08c63c79a2e5efa0b3d7a9e0363b8
mahfujul-alam-anik <mahfujulalam.anik@gmail.com>
2026-02-05 00:10:13 +0600

    updated the next, react version

--- proxy.js
+++ proxy.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   try {
     const res = await fetch(
-      `https://api.ipregistry.co/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`
+      `https://api.ipregistry.co/?key=${process.env.NEXT_PUBLIC_IP_API_KEY}`,
     );
     const data = await res.json();
     console.log("IP Code:", data.location?.country?.code);
